7c6faabbd4e7d57ff33cfd8d8d65eb50edca08d2
Kamranbb <kamrannb@code.edu.az>
2024-02-17 03:53:30 +0400

    Proje dosyası ekle.

---
+++ array.task/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+Console.WriteLine("Hello, World!");
+
+//#region task1
+//int[] numbers = { 2,5,6,9,10,12};
+//int sum = 0;
+//for (int i = 0; i < numbers.Length; i++) 
+//{
+//    sum += numbers[i];
+//}
+//Console.WriteLine(sum);
+//#endregion
+
+
+//#region task2
+//int[] num = { 23, 44, 33, 22, 11 };
+//int max = num[0];
+//for (int i = 0; i < num.Length; i++)
+//{
+//    if (num[i] > max)
+//    {
+//        max = num[i];   
+//    }
+
+//}
+//Console.WriteLine(max);
+//#endregion
+
+
+
+//#region task3 
+//int[] numbers = { 12, 33, 22, 55, 66 };
+//int sum = numbers[0] + numbers[numbers.Length - 1];
+//for (int i = 1; i < numbers.Length; i++)
+//{
+//    break;
+//}
+//Console.WriteLine(sum);
+//#endregion
